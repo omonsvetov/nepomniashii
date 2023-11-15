3345b266150b4614b298135fd3a2b3352dbbff2a
omonsvetov <o.monsvetov@gmail.com>
2023-11-15 21:45:02 +0300

    next-app-f

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { TheHeader } from '@/components/TheHeader'
+import { TheFooter } from '@/components/TheFooter'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <TheHeader />
+        <main className='container'>{children}</main>
+        <TheFooter />
+      </body>
     </html>
   )
 }
